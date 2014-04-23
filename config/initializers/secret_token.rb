c7072872a764517ad4fe9f20919ddce1e6f29eed
Gandhi <ashish.gandhy@gmail.com>
2014-04-23 23:17:18 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Incubus::Application.config.secret_key_base = '0802109317735b74425a323830bf30954e4ac80d46f358e367eec9ecfdb92ca42d0ec334cec2df0c78a761d473193479231eb590f15f550c480980b137bf7bcf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Incubus::Application.config.session_store :cookie_store, key: '_incubus_session'
